2dbbb4d449cdf95509029649f979f67ae3730b95
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-06-23 19:36:18 +0530

    Added theme Reducer

--- src/store/features/theme/themeSlice.ts
+++ src/store/features/theme/themeSlice.ts
@@ -1,11 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+
+type themeState = {
+    theme: string
+}
+
+const initialTheme = (): string => {
+    const saved = localStorage.getItem('chat-theme')
+    return saved as string || "coffee"
+}
+
+const initialState: themeState = {
+    theme: initialTheme()
+}
 
 const themeSlice = createSlice({
-    name:'theme',
-    initialState:[],
-    reducers:{
-        
+    name: 'theme',
+    initialState,
+    reducers: {
+        setTheme: (state, action: PayloadAction<string>) => {
+            state.theme = action.payload;
+            localStorage.setItem("chat-theme", action.payload)
+        }
     }
 })
 
+
+export const { setTheme } = themeSlice.actions
 export default themeSlice.reducer
