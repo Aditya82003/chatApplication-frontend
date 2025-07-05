fa8f62da395ef68464cd8c10d965717f1deb818a
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-07-05 17:49:25 +0530

    added framer motion(animation)

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,20 @@
 import type { FC } from "react"
 import { FiMessageSquare } from "react-icons/fi"
+import { motion } from "motion/react"
 
 const Welcome: FC = () => {
   return (
     <div className="flex flex-1  flex-col gap-6  items-center justify-center h-full w-full p-16 bg-base-100/50">
-      <div className="w-16 h-16 bg-primary/10  rounded-xl flex justify-center items-center hover:bg-primary/20 transition-colors">
+      <motion.div 
+        animate={{y:-50}}
+        transition={{
+          duration:1,
+        repeat:Infinity,
+        type:"spring",
+      repeatType:"mirror"}}
+      className="w-16 h-16 bg-primary/10  rounded-xl flex justify-center items-center hover:bg-primary/20 transition-colors">
         <FiMessageSquare className="size-8 text-primary/80" />
-      </div>
+      </motion.div>
       <div className="text-center w-full max-w-md ">
       <h1 className="font-semibold  text-2xl lg:text-3xl">Welcome to Talksy!</h1>
       <p className="mt-6 text-lg  md:text-xl  text-base-content/60">Select a converstation from the sidebar to start chatting</p>
