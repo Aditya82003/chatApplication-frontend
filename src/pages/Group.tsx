9d6dec50aa151016dc8a395c9efe02e1e68ce6c7
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-07-13 17:41:19 +0530

    Added user join and left notification

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -1,23 +1,65 @@
-import { useState, type FC } from "react"
+import { useEffect, useState, type FC } from "react"
 import { AiOutlineGroup } from "react-icons/ai"
 import { BiGroup } from "react-icons/bi"
 import { v4 as uuidv4 } from 'uuid';
+import { getSocket } from "../lib/socket";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
 
 
 const Group: FC = () => {
+    const {user}=useSelector((state:RootState)=>state.auth)
     const [joined, setJoined] = useState<boolean>(false)
     const [roomId, setRoomId] = useState<string>("")
+    useEffect(() => {
+        const socket = getSocket()
+        if (!socket) return
+
+        const handleUserJoined = (msg: string) => {
+            console.log("user Joined",msg)
+            toast.success(msg)
+        }
+        const handleUSerLeft = (msg:string)=>{
+            console.log("user left",msg)
+            toast.error(msg)
+        }
+        socket.on("user-joined", handleUserJoined)
+        socket.on('user-left',handleUSerLeft
+        )
+
+        return () => {
+            socket.off("user-joined", handleUserJoined)
+            socket.off('user-left',handleUSerLeft)
+        }
+    }, [])
 
     const handleCreateRoom = () => {
-        const newRoomId = uuidv4().slice(0, 6) // short ID
+        const newRoomId = uuidv4().slice(0, 6)
+        const name=user?.fullName
+         const socket = getSocket()
+         setRoomId(newRoomId)
+        if (roomId.trim()) {
+            socket?.emit('join-room', {roomId:newRoomId,name})
+        }
         setJoined(true)
-        setRoomId(newRoomId)
-        console.log(roomId)
     }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        setJoined(true)
+        const socket = getSocket()
+        const name=user?.fullName
+        if (roomId.trim()) {
+            socket?.emit('join-room', {roomId,name})
+            setJoined(true)
+        }
+    }
+    const handleLeaveRoom = () => {
+        const name=user?.fullName
+        const socket = getSocket()
+        socket?.emit('leave-room', {roomId,name})
+        setJoined(false)
+
     }
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setRoomId(e.target.value)
@@ -29,7 +71,7 @@ const Group: FC = () => {
                 <div className="flex justify-center items-center h-full w-full ">
                     <div className="flex items-center justify-center max-w-2xl w-full h-[500px] bg-base-300 rounded-xl">
                         <h1>Group id {roomId}</h1>
-                        <button className="btn btn-md" onClick={()=>setJoined(false)}>Leave Group</button>
+                        <button className="btn btn-md" onClick={handleLeaveRoom}>Leave Group</button>
                     </div>
                 </div>
             </div>
